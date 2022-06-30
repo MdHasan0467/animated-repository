765bfc602e73251b47b3385deac3837b1c337803
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-06-30 20:29:07 +0600

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+  //------------Start Menu Bar From Here
+
+var navLinks = document.getElementById("navLinks");
+
+
+  function showMenu() {
+  navLinks.style.top = "100px"; //beacause of we want to change the right value
+  }
+
+  function hideMenu() {
+  navLinks.style.top = "-200px"; //beacause of we want to change the right value
+}
+  
+//----------------End Menu Bar Here
